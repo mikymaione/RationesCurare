cdbce79ff4dddb95614be4c0a5a6264d25344be5
Michele Maione <michele.maione@eng.it>
2021-07-06 09:50:26 +0200

    progetto vuoto

---
+++ src/main/java/it/maionemiky/RationesCurare/RationesCurareApplication.java
@@ -0,0 +1,13 @@
+package it.maionemiky.RationesCurare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RationesCurareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RationesCurareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/maionemiky/RationesCurare/RationesCurareApplicationTests.java
@@ -0,0 +1,13 @@
+package it.maionemiky.RationesCurare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RationesCurareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
