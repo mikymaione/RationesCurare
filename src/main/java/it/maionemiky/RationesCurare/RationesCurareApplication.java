2e8f1ad88511a13535e1d18305c1b8fdb93b9a60
Michele Maione <michele.maione@eng.it>
2021-07-07 10:16:39 +0200

    Memory DB

--- src/main/java/it/maionemiky/RationesCurare/RationesCurareApplication.java
+++ src/main/java/it/maionemiky/RationesCurare/RationesCurareApplication.java
@@ -2,17 +2,9 @@ package it.maionemiky.RationesCurare;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class RationesCurareApplication {
-
-    @RequestMapping("/")
-    @ResponseBody
-    String home() {
-        return "Welcome to RationesCurare";
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(RationesCurareApplication.class, args);
