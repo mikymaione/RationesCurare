e93f0dfbdcf86e09149da980a91145f58c72f2e9
Michele Maione <michele.maione@eng.it>
2021-07-07 10:06:56 +0200

    Aggiunto welcome msg

--- src/main/java/it/maionemiky/RationesCurare/RationesCurareApplication.java
+++ src/main/java/it/maionemiky/RationesCurare/RationesCurareApplication.java
@@ -2,12 +2,20 @@ package it.maionemiky.RationesCurare;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class RationesCurareApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RationesCurareApplication.class, args);
-	}
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Welcome to RationesCurare";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(RationesCurareApplication.class, args);
+    }
 
 }
