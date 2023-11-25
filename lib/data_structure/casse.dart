d1ebd8dbb984c54af5f723e2b46ba82067b155d2
Michele Maione <mikymaione@hotmail.it>
2023-11-25 10:58:24 +0100

    Casse

---
+++ lib/data_structure/casse.dart
@@ -0,0 +1,11 @@
+class Casse {
+  final String nome, imgName, valuta;
+  final bool nascondi;
+
+  const Casse({
+    required this.nome,
+    required this.imgName,
+    required this.valuta,
+    required this.nascondi,
+  });
+}
