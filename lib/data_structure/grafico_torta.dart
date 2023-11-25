15fcf37d3c5a9a547c361b9e4a491bfd94300f38
Michele Maione <mikymaione@hotmail.it>
2023-11-25 17:03:02 +0100

    GraficoTorta

---
+++ lib/data_structure/grafico_torta.dart
@@ -0,0 +1,8 @@
+class GraficoTorta {
+  final String macroArea, soldi;
+
+  const GraficoTorta({
+    required this.macroArea,
+    required this.soldi,
+  });
+}
---
+++ lib/store/store_grafico_torta.dart
@@ -0,0 +1,61 @@
+import 'package:rationes_curare/data_structure/grafico_torta.dart';
+import 'package:rationes_curare/data_structure/valute.dart';
+import 'package:rationes_curare/store/query_manager.dart';
+import 'package:rationes_curare/store/store_base.dart';
+import 'package:sqlite3/sqlite3.dart';
+
+class StoreGraficoTorta extends StoreBase<Valute> {
+  const StoreGraficoTorta({
+    required super.db,
+    super.deleteQuery,
+    super.insertQuery,
+    super.updateQuery,
+    super.getQuery,
+    super.listQuery,
+  });
+
+  @override
+  Valute dbToEntity(Row r) => throw UnimplementedError();
+
+  @override
+  List<Object?> entityToDb(Valute e) => throw UnimplementedError();
+
+  Future<List<GraficoTorta>> grafico({
+    required int sPos,
+    required DateTime dataDa,
+    required DateTime dataA,
+  }) async =>
+      [
+        for (final r in db.select(
+          await QueryManager.getSql(Queries.Movimenti_GraficoTorta),
+          [
+            sPos,
+            sPos,
+            sPos,
+            dataDa,
+            dataA,
+          ],
+        )) ...[
+          GraficoTorta(
+            macroArea: r['MacroArea'],
+            soldi: r['Soldini_TOT'],
+          ),
+        ],
+      ];
+
+  Future<double> saldo({
+    required int sPos,
+    required DateTime dataDa,
+    required DateTime dataA,
+  }) async =>
+      db.select(
+        await QueryManager.getSql(Queries.Movimenti_GraficoTortaSaldo),
+        [
+          sPos,
+          sPos,
+          sPos,
+          dataDa,
+          dataA,
+        ],
+      ).single['Soldini_TOT'];
+}
