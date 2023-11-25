6241e2cfe9d1af8e8a3d66bb8b7fdc0794691d5b
Michele Maione <mikymaione@hotmail.it>
2023-11-25 10:53:34 +0100

    Periodicita

--- lib/data_structure/movimenti_tempo.dart
+++ lib/data_structure/movimenti_tempo.dart
@@ -1,12 +1,28 @@
+enum Periodicita {
+  giornaliero(code: 'G', label: 'giornaliero'),
+  mensile(code: 'M', label: 'mensile'),
+  bimestrale(code: 'B', label: 'bimestrale'),
+  trimestrale(code: 'T', label: 'trimestrale'),
+  quadrimestrale(code: 'Q', label: 'quadrimestrale'),
+  semestrale(code: 'S', label: 'semestrale'),
+  annuale(code: 'A', label: 'annuale');
+
+  final String code, label;
+
+  const Periodicita({
+    required this.code,
+    required this.label,
+  });
+}
+
 class MovimentiTempo {
   final int id;
   final String nome, tipo, descrizione, macroArea;
   final double soldi;
 
   final int numeroGiorni;
-  final String giornoDelMese, tipoGiorniMese;
-
-  final DateTime partendoDalGiorno, scadenza;
+  final Periodicita tipoGiorniMese;
+  final DateTime giornoDelMese, partendoDalGiorno, scadenza;
 
   const MovimentiTempo({
     required this.id,
