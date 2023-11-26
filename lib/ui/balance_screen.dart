04c500b3d00cadfe842ea6ef707171dc7cbe889c
Michele Maione <mikymaione@hotmail.it>
2023-11-26 12:47:03 +0100

    balance screen

--- lib/ui/balance_screen.dart
+++ lib/ui/balance_screen.dart
@@ -6,34 +6,33 @@ Copyright (c) 2023 Michele Maione
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
-import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:rationes_curare/ui/balance_screen.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:rationes_curare/ui/base/screen.dart';
+//import 'package:sqlite3/sqlite3.dart';
 
-class RationesCurare extends StatelessWidget {
-  const RationesCurare({super.key});
+class BalanceScreen extends StatefulWidget {
+  const BalanceScreen({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _BalanceScreenState();
+}
+
+class _BalanceScreenState extends State<BalanceScreen> {
+  //late final Database db;
+
+  @override
+  void initState() {
+    super.initState();
+    //db = sqlite3.open('db.rqd8');
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Rationes Curare',
-      debugShowCheckedModeBanner: false,
-      localizationsDelegates: const [
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: const [
-        Locale('it'),
-      ],
-      themeMode: ThemeMode.system,
-      theme: ThemeData(
-        brightness: Brightness.light,
+    return const Screen(
+      title: 'Worklick',
+      body: Column(
+        children: [],
       ),
-      darkTheme: ThemeData(
-        brightness: Brightness.dark,
-      ),
-      home: const BalanceScreen(),
     );
   }
 }
