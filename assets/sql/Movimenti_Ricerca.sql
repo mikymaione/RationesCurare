44d77ea3955966dd24bb63b269634d92a39c9a6e
Michele Maione <mikymaione@hotmail.it>
2023-12-02 16:20:12 +0100

    MovimentiRicerca bug where

--- assets/sql/Movimenti_Ricerca.sql
+++ assets/sql/Movimenti_Ricerca.sql
@@ -2,10 +2,10 @@ select
 	*
 from Movimenti
 where
-	(('Saldo' = @tipo) or (tipo = @tipo)) and
+	(('Saldo' = @tipo1) or (tipo = @tipo2)) and
 	(descrizione like @descrizione) and
 	(MacroArea like @MacroArea) and	
-	((0 = @bSoldi) or (soldi between @SoldiDa and @SoldiA)) and
-	((0 = @bData) or (data between @DataDa and @DataA))
+	((false = @bSoldi) or (soldi between @SoldiDa and @SoldiA)) and
+	((false = @bData) or (data between @DataDa and @DataA))
 order by
 	data desc
