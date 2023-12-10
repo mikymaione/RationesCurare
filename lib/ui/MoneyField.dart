5e1504d637aa77b96f177c1f27aee94f7280aec6
Michele Maione <mikymaione@hotmail.it>
2023-12-10 17:51:33 +0100

    add calc to moneyField

--- lib/ui/MoneyField.dart
+++ lib/ui/MoneyField.dart
@@ -8,6 +8,7 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 */
 import 'package:currency_text_input_formatter/currency_text_input_formatter.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_simple_calculator/flutter_simple_calculator.dart';
 import 'package:rationes_curare/utility/generic_controller.dart';
 import 'package:intl/intl.dart';
 
@@ -24,6 +25,8 @@ class MoneyField extends StatefulWidget {
 }
 
 class _MoneyFieldState extends State<MoneyField> {
+  final textEditingController = TextEditingController();
+
   late final languageCode = Localizations.localeOf(context).languageCode;
 
   late final numberFormat = NumberFormat.simpleCurrency(locale: languageCode);
@@ -35,15 +38,55 @@ class _MoneyFieldState extends State<MoneyField> {
     turnOffGrouping: false,
   );
 
+  void setValue() {
+    if (widget.controller.value != null) {
+      textEditingController.text = formatter.format(widget.controller.value!.toStringAsFixed(2));
+    }
+  }
+
+  Future<void> showCalc() async {
+    final c = CalcController();
+
+    await showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      builder: (context) => SizedBox(
+        height: MediaQuery.of(context).size.height * 0.75,
+        child: SimpleCalculator(
+          autofocus: true,
+          value: widget.controller.value ?? 0,
+          controller: c,
+          numberFormat: numberFormat,
+        ),
+      ),
+    );
+
+    final d = c.value;
+    c.dispose();
+
+    if (d != null) {
+      widget.controller.value = d;
+      setValue();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    setValue();
+
     return TextFormField(
-      initialValue: widget.controller.value == null ? null : formatter.format(widget.controller.value!.toStringAsFixed(2)),
+      controller: textEditingController,
       onChanged: (s) => widget.controller.value = formatter.getUnformattedValue().toDouble(),
       keyboardType: TextInputType.number,
       inputFormatters: [
         formatter,
       ],
+      decoration: InputDecoration(
+        suffixIcon: IconButton(
+          icon: const Icon(Icons.calculate),
+          onPressed: () => showCalc(),
+        ),
+      ),
     );
   }
 }
