3bcf0a527f9b5f01443c5260091a59759d1cfceb
Michele Maione <mikymaione@hotmail.it>
2024-01-06 17:55:56 +0100

    feat: add Back Button Callback

--- lib/ui/base/screen.dart
+++ lib/ui/base/screen.dart
@@ -7,17 +7,20 @@ Permission is hereby granted, free of charge, to any person obtaining a copy of 
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 import 'package:flutter/material.dart';
+import 'package:rationes_curare/utility/callbacks.dart';
 
 class Screen extends StatelessWidget {
   final String title;
   final Widget child;
   final List<Widget>? actions;
+  final Generic0ParamCallback<dynamic>? onBack;
 
   const Screen({
     super.key,
     required this.title,
     required this.child,
     this.actions,
+    this.onBack,
   });
 
   @override
@@ -29,7 +32,15 @@ class Screen extends StatelessWidget {
       ),
       body: SizedBox(
         width: double.infinity,
-        child: child,
+        child: PopScope(
+          canPop: false,
+          onPopInvoked: (didPop) {
+            if (!didPop) {
+              Navigator.pop(context, onBack?.call());
+            }
+          },
+          child: child,
+        ),
       ),
     );
   }
