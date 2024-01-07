6f6d593e208c007d88dd1bc6fd8433b3cce0e985
Michele Maione <mikymaione@hotmail.it>
2024-01-07 11:54:32 +0100

    feat: add delete transaction functionality

--- lib/ui/transaction_screen.dart
+++ lib/ui/transaction_screen.dart
@@ -6,6 +6,7 @@ Copyright (c) 2023 Michele Maione
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
+import 'package:confirm_dialog/confirm_dialog.dart';
 import 'package:dart_casing/dart_casing.dart';
 import 'package:flutter/material.dart';
 import 'package:rationes_curare/data_structure/movimenti.dart';
@@ -106,7 +107,31 @@ class _TransactionScreenState extends State<TransactionScreen> {
     }
   }
 
-  void _delete() {}
+  Future<void> _delete() async {
+    final canContinue = await confirm(
+      context,
+      content: const Text('Would you like to delete?'),
+    );
+
+    if (canContinue) {
+      final store = StoreMovimenti(db: widget.db);
+
+      try {
+        final id = widget.transaction!.id!;
+
+        await store.delete(id);
+
+        if (mounted) {
+          Msg.showOk(context, 'Deleted');
+          Navigator.pop(context, true);
+        }
+      } catch (e) {
+        if (mounted) {
+          Msg.showError(context, e);
+        }
+      }
+    }
+  }
 
   Future<List<String>> _casse() async {
     final store = StoreCasse(db: widget.db);
@@ -316,7 +341,7 @@ class _TransactionScreenState extends State<TransactionScreen> {
                   const SizedBox(height: 16),
                   Row(
                     children: [
-                      if (widget.transaction != null) ...[
+                      if (widget.transaction?.id != null) ...[
                         ElevatedButton(
                           onPressed: () => _delete(),
                           style: ElevatedButton.styleFrom(
